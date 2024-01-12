f7718e4da28079de4faa33f864eb5a14c4a5c63c
Emzzy241 <emzzyoluwole@gmail.com>
2024-01-12 12:57:50 +0100

    adding the UI for game

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -6,9 +6,41 @@ namespace RockPaperScissors.Models
     
     public class Program
     {
-        static void Main()
+        public static void Main()
         {
-            Console.WriteLine("Halo there, lets play th eRock Paper Scissors game together.");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Welcome to the Rock Paper Scissors Game");
+            Console.WriteLine("---------------------------------------");
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Here are the rules for the game: ");
+
+            static void GameRules()
+            {
+                
+                Console.WriteLine("Enter 'Rock' to chose Rock");
+                Console.WriteLine("Enter 'Scissors' to chose Scissors");
+                Console.WriteLine("Enter 'Paper' to chose Paper");
+
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Enter 'Y' to continue or 'N' to end game");
+            string userContinue = Console.ReadLine();
+
+            switch (userContinue)
+            {
+                case "Y":
+                    Console.WriteLine("Okay, enter any of the following");
+                    break;
+                default:
+                    break;
+            }
+            
+
+
         }
     }
 }
